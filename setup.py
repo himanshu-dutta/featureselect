ff0e4473f3c7301a3ad84331443fa13dd03233d6
himanshu-dutta <meet.himanshu.dutta@gmail.com>
2020-11-06 21:39:41 +0530

    modified classifiers

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     long_description=long_description,
     # long_description_content_type="text/markdown",
     classifiers=[
-        "Development Status :: 1 - Pre-Alpha",
+        "Development Status :: 2 - Pre-Alpha",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
